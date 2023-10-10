bf1463f912395978d5c89628f5ef0bffa6183863
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2023-10-10 16:14:44 +0400

    added declaration of operator(), +=, +, -=, -, *, /, and added declaration of track

--- Matrix.h
+++ Matrix.h
@@ -7,18 +7,21 @@ class Matrix
 	int _width;
 	int _height;
 public:
-	Matrix();
-	Matrix(int width, int height);
-	Matrix(const Matrix& other);
-	~Matrix();
+	Matrix();										//конструктор по умолчанию
+	Matrix(int width, int height);					//конструктор с параметрами
+	Matrix(int width, int height);					//конструктор со случайными параметрами
+	Matrix(const Matrix& other);					//конструктор копирования
+	~Matrix();										//деструктор
 
-	Matrix& operator+=(const Matrix& other);
-	Matrix operator+(const Matrix other) const;
-	Matrix& operator-=(const Matrix& other);
-	Matrix operator-(const Matrix other) const;
-	Matrix operator*(const Matrix other) const;
-	Matrix operator*(const int scalar) const;
 
-	
+	T& operator()(int i, int j);
+	Matrix& operator+=(const Matrix& other);		//оператор +=
+	Matrix operator+(const Matrix other) const;		//оператор сложения
+	Matrix& operator-=(const Matrix& other);		//оператор -=
+	Matrix operator-(const Matrix other) const;		//оператор вычитания
+	Matrix operator*(const Matrix other) const;		//оператор умножения матриц
+	Matrix operator*(const int scalar) const;		//оператор умножения на скаляр
+	Matrix operator/(const int scalar) const;		//оператор деления матрицы на скаляр
+	T track(const Matrix& other);					//вычисление следа
 };
 
